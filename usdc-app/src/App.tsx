ed4ee62ff57f0e030bac364fc94adeb04a1cea53
nathanralph01 <nathan.ralph122@gmail.com>
2025-02-26 12:46:06 -0500

    Added data

--- usdc-app/src/App.tsx
+++ usdc-app/src/App.tsx
@@ -1,28 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Table from "./components/Table";
+import { ethers } from "ethers";
+import aaveABI from './ABI/AaveABI.json';
 
-import './App.css'
+const INFURA_API_KEY = import.meta.env.VITE_INFURA_API_KEY;
+
+
 
 function App() {
-  const [totalSupply, setTotalSupply] = useState(0)
-  const [supplyApy, setSupplyApy] = useState(0)
-  const [totalBorrowed, setTotalBorrowed] = useState(0)
-  const [borrowApy, setBorrowApy] = useState(0)
+  const [usdcData, setUsdcData] = useState({
+    totalSupplied: 0,
+    supplyAPY: 0,
+    totalBorrowed: 0,
+    borrowAPY: 0,
+  });
 
+  const AAVE_CONTRACT_ADDRESS = "0x497a1994c46d4f6C864904A9f1fac6328Cb7C8a6";
+  // Relevent parts of ABI
+  const USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Connect to network
+        const provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`)
+        const contract = new ethers.Contract(
+          AAVE_CONTRACT_ADDRESS,
+          aaveABI,
+          provider
+        );
+        const reserveData = await contract.getReserveData(
+          USDC_CONTRACT_ADDRESS
+        );
+        setUsdcData({
+          totalSupplied: Number(ethers.formatUnits(reserveData.totalAToken, 6)),
+          supplyAPY: Number(reserveData.liquidityRate) / 1e25, // Divide by 1e25 as it is a ray (27 decimal places)
+          totalBorrowed: Number(ethers.formatUnits(reserveData.totalVariableDebt, 6)),
+          borrowAPY: Number(reserveData.variableBorrowRate) / 1e25 // Divide by 1e25 as it is a ray (27 decimal placse), 
+        });
 
+        // Important data: Total borrowed = getTotalDebt, Total Supply = getATokenTotalSupply
+        // APY = Annual percentage yield supply APY is liquidity rate
+        // Total borrowed APY is the variable borrow rate. Use the getReserveData function for all data
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <h3>USDC Market</h3>
-        <p>Total supplied: {totalSupply} </p>
-        <p>Supply APY: {supplyApy} </p>
-        <p>Total Borrowed: {totalBorrowed} </p>
-        <p>Borrowed APy: {borrowApy} </p>
+      <div className="App">
+        <h3>USDC Market Data</h3>
+        <Table data={usdcData} />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ usdc-app/src/components/Table.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface TableProps {
+  data: { [key: string]: number};
+}
+function Table(props: TableProps) {
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>Total Supplied</th>
+          <th>Supply APY %</th>
+          <th>Total Borrowed</th>
+          <th>Borrowed APY %</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{props.data.totalSupplied.toFixed(2)}</td>
+          <td>{props.data.supplyAPY.toFixed(2)}</td>
+          <td>{props.data.totalBorrowed.toFixed(2)}</td>
+          <td>{props.data.borrowAPY.toFixed(2)}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Table
